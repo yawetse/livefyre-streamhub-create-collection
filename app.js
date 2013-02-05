cf3ff2c0cb899e5df6f8bea2fb4116e4e6f9d12b
Yaw Etse <yaw.etse@gmail.com>
2013-02-05 15:18:40 -0500

    fixed posting

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var http = require('http'),
 	jwt  = require('jwt-simple'),
+	querystring = require('querystring'),
     argv = require('optimist')
-    	   .usage('Usage: $0 -aid [article id] -title [title] -net [network] -site [site id] -secret [site secret] -url [url base] -save [true/false]')
-    	   .demand(['aid','title','net','site','secret','url'])
-    	   .default('save', true)
-    	   .alias('aid','a')
-    	   .describe('aid','livefyre artcile id')
+    	   .usage('Usage: $0 -col [article id] -title [title] -net [network] -site [site id] -secret [site secret] -url [url base] -save [true/false]')
+    	   .demand(['title','net','site','secret','url','col'])
     	   .alias('title','t')
-    	   .describe('title','livefyre collection title')
+    	   .describe('title','livefyre collection title')   	   
     	   .alias('net','n')
     	   .describe('net','livefyre network, ex: labs-t402.fyre.co')
     	   .alias('site','s')
@@ -16,44 +14,41 @@ var http = require('http'),
     	   .describe('secret','livefyre site secret, ex: [user token]')
     	   .alias('url','u')
     	   .describe('url','url base, ex: http://demos.livefyre.com/labs-t402/')
-    	   .describe('save','create(true) test(false)')
+       	   .alias('save','e')
+		   .describe('save','create(true) test(false)')       	   
+		   .alias('col','c')
+		   .describe('col','livefyre artcile/collection id')   
     	   .argv;
+
 
 var __NETWORK = argv.net; //'labs-t402.fyre.co',
 	__SITE_ID = argv.site; //303827,
 	__SITE_SECRET = argv.secret; //'/=',
 	__URL_BASE = argv.url; //'http://demos.livefyre.com/labs-t402/',
-	__SAVE = argv.save; //true
+	__SAVE = (argv.save == 'f' || argv.save == 'false' || argv.save == false ) ? false : true; //true
+
 
 
 if( __NETWORK 
 	&& __SITE_ID 
 	&& __SITE_SECRET 
 	&& __URL_BASE 
-	&& __SAVE
-	&& argv.aid 
+	&& argv.col 
 	&& argv.title){
 
 	var collection_data = {
-		article_id: argv.aid,
+		article_id: argv.col,
 		title: argv.title
 	}
+
+	// console.log(JSON.stringify(collection_data))
 
 	create_collection(collection_data)
 }
 else{
 	console.log("missing data")
-
 }
-// if __name__ == "__main__":
-// 	cols = [
-// 	("Media", "media_0")
-// 	]
-// 	col_objs = [dict(title=k, article_id=v) for (k,v) in cols]
-// 	create_collections(col_objs)
 
-
- 
 function collection_meta_jwt(site_secret, article_id, title, url, tags){
     // Create JSON Obj
     data = {
@@ -78,48 +73,53 @@ function create_collections(infos){
 }
 
 function create_collection(info){
-	var article_id = info['article_id']
-	var title = info['title']
-	var url = __URL_BASE + article_id
-	var meta_jwt = collection_meta_jwt(__SITE_SECRET, article_id, title, url)
-	var format = 'json'
-	if(__SAVE){
-		// post_url = "http://quill."+__NETWORK+"/api/v3.0/site/"+__SITE_ID+"/collection/create";
-		// data_str = json.dumps({"collectionMeta": meta_jwt})
-		// print ("Making request to {url} with data={data}".format(url=post_url, data=data_str))
-		// resp = requests.post(post_url, data=data_str)
-		// print "Response: {resp} {resptext}".format(
-		// 	resp=resp,
-		// 	resptext=resp.text)
-
-
-		var http = require('http');
-
-		var options = {
-		  host: "quill."+__NETWORK,
-		  path: "/api/v3.0/site/"+__SITE_ID+"/collection/create",
-		  port: '80',
-		  //This is the only line that is new. `headers` is an object with the headers to request
-		  // headers: {'custom': 'Custom Header Demo works'}
+	var post_data = {
+			article_id : info['article_id'],
+			title: info['title'],
+			url: __URL_BASE + info['article_id'],
+			meta_jwt : collection_meta_jwt(__SITE_SECRET, info['article_id'], info['title'], __URL_BASE + info['article_id'])		
+		}
+	var post_data_string = querystring.stringify(post_data);
+	var http = require('http');
+	var post_options = {
+		  	host: "quill."+__NETWORK,
+		  	path: "/api/v3.0/site/"+__SITE_ID+"/collection/create",
+		  	port: '80',
+			method: 'POST',
+			headers: {
+			  	'Content-Type': 'application/json',
+			  	'Content-Length': post_data_string.length
+			}
 		};
 
-		callback = function(response) {
-		  var str = ''
-		  response.on('data', function (chunk) {
-		    str += chunk;
-		  });
 
-		  response.on('end', function () {
-		    console.log(str);
-		  });
+	if(__SAVE){
+		callback = function(response) {
+			var str = ''
+
+			response.setEncoding('utf-8');
+
+			response.on('data', function (chunk) {
+			str += chunk;
+			});
+
+			response.on('end', function () {
+			console.log(str);
+			});
 		}
 
-		var req = http.request(options, callback);
+		var req = http.request(post_options, callback);
+		// req.write(post_data);
+		console.log("\r\n \r\n \r\n --testing json posting--")
+		console.log(JSON.stringify(post_data))
+		console.log("--end testing json posting-- \r\n \r\n \r\n")
+
+		req.write(JSON.stringify(post_data))
 		req.end();
 	}
 	else{
 		console.log(
-			"Creating Collection for article_id="+article_id+", title="+title+", url="+url
+			"Creating Collection for article_id="+post_data.article_id+", title="+post_data.title+", url="+post_data.url
 		); 
 	}
 }
